ee3b3858146d4aaaeded259311e032105680f197
heejin <heejin2dat@naver.com>
2024-11-08 01:08:38 +0900

    form.py 완성

--- photo/urls.py
+++ photo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.photo_list, name='photo_list')
+  path('', views.photo_list, name='photo_list'),
+  path('photo/<int:pk>/', views.photo_detail, name='photo_detail')
 ]
